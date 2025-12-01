e42878bc0dad82056ad065ba78ff16247e95fc18
Thiago Fernandes <4077420+thiagovf@users.noreply.github.com>
2025-12-01 12:10:29 -0300

    Const vs Var

---
+++ Aula01/src/teste3.ts
@@ -0,0 +1,27 @@
+// LET vs. CONST
+let idade: number = 25;
+const nome: string = "João";
+
+idade = 26; // Válido
+// nome = "Maria"; // Inválido, causa erro  porque CONST não pode ser reatribuído.
+
+console.log(`Nome: ${nome}, Idade: ${idade}`);
+
+// Escopo de bloco
+if(true) {
+    let cidade: string = "São Paulo";
+    console.log(`Cidade dentro do bloco: ${cidade}`);
+}
+
+// console.log(`Cidade fora do bloco: ${cidade}`); // Erro! cidade não está acessível fora do bloco.
+
+// Uso de CONST com objetos
+const pessoa = {
+    nome: "Ana",
+    idade: 30
+};
+
+pessoa.idade = 31; // Válido, podemos modificar propriedades do objeto
+// pessoa = { nome: "Carlos", idade: 28 }; // Inválido, não podemos reatribuir o objeto
+
+console.log(`Pessoa: ${pessoa.nome}, Idade: ${pessoa.idade}`); 
